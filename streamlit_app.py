85339d1e29c718beeaabce8fbe99cbc243c81ea5
saqired <jeffreychong2727@gmail.com>
2025-07-15 08:40:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title("📊 Live Dashboard Example")
 
 while True:
     # load data from file or API
-    df = pd.read_csv("live_data.csv")
+    df = pandas.read_csv("live_data.csv")
     
     st.line_chart(df)
     
